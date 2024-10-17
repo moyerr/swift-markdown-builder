19c55dbe5c2f2118adefa75dfdcc255d718eb4e6
Robbie Moyer <rmoyer08@gmail.com>
2024-10-17 15:32:44 -0400

    Update readme

--- Sources/create-readme/main.swift
+++ Sources/create-readme/main.swift
@@ -6,15 +6,61 @@ let readme = Document {
         "Swift Markdown Builder"
     }
 
+    """
+    Swift `MarkdownBuilder` is a package that provides a result-builder DSL \
+    for [`swift-markdown`](https://github.com/swiftlang/swift-markdown). The \
+    [documentation](https://swiftpackageindex.com/swiftlang/swift-markdown/documentation/markdown) \
+    for the `swift-markdown` package provides detailed examples for creating \
+    Markdown documents programmatically. This package builds on that foundation \
+    by providing a more natural DSL that will feel familiar to users of SwiftUI and \
+    other result builder DSLs.
+    """
+
+    Heading(level: 2) {
+        "Usage Examples"
+    }
+
+    Heading(level: 4) {
+        "Using control flow statements"
+    }
+
     Paragraph {
+        "The DSL allows you to create Markdown documents using standard Swift control flow statments. For example, you can generate an ordered list using a for-loop: "
+    }
+
+    CodeBlock(language: "swift") {
         """
-        Swift `MarkdownBuilder` is a Swift package that provides a result-builder DSL \
-        on top of [`swift-markdown`](https://github.com/swiftlang/swift-markdown).
+        let groceries = [
+            "Milk",
+            "Eggs",
+            "Bread",
+        ]
+        
+        let groceryList = Document {
+            Heading(level: 2) {
+                "Grocery List"
+            }
+        
+            OrderedList {
+                for item in groceries {
+                    ListItem(Text(item))
+                }
+            }
+        }
         """
     }
 
-    CodeBlock {
-        "version 2"
+    Heading(level: 4) {
+        "TODO: More examples coming soon"
+    }
+
+    BlockQuote {
+        """
+        [!NOTE]
+        This README was generated using `MarkdownBuilder`. Do not edit it directly. \
+        Instead, edit the file at `Sources/create-readme/main.swift` and run \
+        `swift run create-readme` to regenerate it.
+        """
     }
 }.format()
 
